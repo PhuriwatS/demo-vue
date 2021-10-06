027e1104195e8eeb34d6f4c00dca4f8dd6a19bb2
phuriwats <pond.sunday@hotmail.com>
2021-10-06 13:15:38 +0700

    Working with native event object

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ Vue.createApp({
     return {
       vueLink: 'https://vuejs.org/',
       outputHtml: '<h2>Sample HTML Header</h2>',
-      goals: [],
-      enteredValue: ''
+      name: '',
     };
   },
   methods: {
-    addGoal() {
-      this.goals.push(this.enteredValue);
-      this.enteredValue = '';
+    setName(event) {
+      this.name = event.target.value;
     }
   }
 }).mount('#app');
