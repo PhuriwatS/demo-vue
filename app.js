c7dc0e176e1154c0e78d90144b65a169f008a6f6
phuriwats <pond.sunday@hotmail.com>
2021-10-06 13:58:17 +0700

    Two-way binding with v-model

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 Vue.createApp({
   data() {
     return {
-      vueLink: 'https://vuejs.org/',
-      outputHtml: '<h2>Sample HTML Header</h2>',
-      name: '',
+      name: 'John',
     };
   },
   methods: {
-    setName(event, lastname) {
-      this.name = `${event.target.value} ${lastname}`;
+    outputFullname() {
+      return `${this.name} Doe`;
+    },
+    resetInput() {
+      this.name = 'John';
     }
   }
 }).mount('#app');
