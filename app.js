daf07e7284c96ceb0080c6730aaaaa2825df7fcb
phuriwats <pond.sunday@hotmail.com>
2021-10-06 20:49:14 +0700

    Introducing components

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      isDetailVisible: false,
       friends: [
         {
           id: "manuel",
@@ -16,6 +15,32 @@ const app = Vue.createApp({
           email: "julie@gmail.com",
         },
       ],
+    };
+  },
+});
+
+app.component("friend-contact", {
+  template: `
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails">
+        {{ isDetailVisible ? "Hide" : "Show"}} Details
+      </button>
+      <ul v-if="isDetailVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+      </ul>
+    </li>
+  `,
+  data() {
+    return {
+      isDetailVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "0812223333",
+        email: "manuel@gmail.com",
+      },
     };
   },
   methods: {
