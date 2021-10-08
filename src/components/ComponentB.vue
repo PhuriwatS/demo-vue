887f5121efd34de602e7bb6115bf1872da8b09cb
phuriwats <pond.sunday@hotmail.com>
2021-10-08 09:53:19 +0700

    Using v-model with Checkboxes and Radiobuttons

--- src/components/ComponentB.vue
+++ src/components/ComponentB.vue
@@ -1,8 +1,36 @@
 <template>
   <p>Component B</p>
-  <input type="text" />
+  <div>
+    <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
+    <label for="jack">Jack</label>
+    <input type="checkbox" id="john" value="John" v-model="checkedNames" />
+    <label for="john">John</label>
+    <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
+    <label for="mike">Mike</label>
+  </div>
+  <div>
+    <input type="radio" id="one" value="One" v-model="picked" />
+    <label for="one">One</label>
+    <input type="radio" id="two" value="Two" v-model="picked" />
+    <label for="two">Two</label>
+  </div>
+  <p>Checked names: {{ checkedNames }}</p>
+  <p>Picked: {{ picked }}</p>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      checkedNames: [],
+      picked: "One",
+    };
+  },
+};
 </script>
+
+<style scoped>
+div {
+  margin-bottom: 1rem;
+}
+</style>
