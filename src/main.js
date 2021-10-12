709e02c41c262e8450a83fdfd80917ecb7041d66
phuriwats <pond.sunday@hotmail.com>
2021-10-12 10:12:26 +0700

    Setup new structure

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-import { createApp } from "vue";
+import { createApp } from 'vue';
 
-import App from "./App.vue";
-import router from "./router.js";
+import App from './App.vue';
 
 const app = createApp(App);
 
-app.use(router);
-
-app.mount("#app");
+app.mount('#app');
