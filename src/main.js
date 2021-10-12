d2b748f48fe4c49dd8b17f8157c92618ed21c490
phuriwats <pond.sunday@hotmail.com>
2021-10-12 19:49:50 +0700

    Organizing Store with Modules

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createStore } from "vuex";
 
 import App from "./App.vue";
 
-const store = createStore({
+const counterModule = {
   state() {
     return {
       counter: 0,
@@ -28,6 +28,35 @@ const store = createStore({
     normalizedCounter(state, getters) {
       const finalCounter = getters.finalCounter;
       return finalCounter > 20 ? 20 : finalCounter;
+    },
+  },
+};
+
+const store = createStore({
+  modules: {
+    counter: counterModule,
+  },
+  state() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  mutations: {
+    setAuth(state, payload) {
+      state.isLoggedIn = payload.isAuth;
+    },
+  },
+  actions: {
+    login(context) {
+      context.commit("setAuth", { isAuth: true });
+    },
+    logout(context) {
+      context.commit("setAuth", { isAuth: false });
+    },
+  },
+  getters: {
+    userIsAuthenticated(state) {
+      return state.isLoggedIn;
     },
   },
 });
