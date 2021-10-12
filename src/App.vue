28a834ac3796218641278f411eb02b3cdc221aa5
phuriwats <pond.sunday@hotmail.com>
2021-10-12 12:11:23 +0700

    Mutations

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
   <base-container title="Vuex">
     <TheCounter />
-    <button @click="addOne">Add 1</button>
+    <ChangeCounter />
   </base-container>
 </template>
 
 <script>
 import BaseContainer from "./components/BaseContainer.vue";
 import TheCounter from "./components/TheCounter.vue";
+import ChangeCounter from "./components/ChangeCounter.vue";
 
 export default {
   components: {
     BaseContainer,
     TheCounter,
-  },
-  methods: {
-    addOne() {
-      this.$store.state.counter++;
-    },
+    ChangeCounter,
   },
 };
 </script>
